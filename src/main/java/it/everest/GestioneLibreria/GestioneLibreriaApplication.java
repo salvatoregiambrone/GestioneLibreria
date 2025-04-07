5b12960ea4b5989d1693f005441c63ad9f9b1a6e
salvatoregiambrone <48088908+salvatoregiambrone@users.noreply.github.com>
2025-04-07 14:26:19 +0200

    Add files via upload
    Primo commit scheletro progetto

---
+++ src/main/java/it/everest/GestioneLibreria/GestioneLibreriaApplication.java
@@ -0,0 +1,13 @@
+package it.everest.GestioneLibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneLibreriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/everest/GestioneLibreria/GestioneLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.everest.GestioneLibreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
